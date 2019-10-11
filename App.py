db32fd9003b002e22272bc7fe36a6bc8c09a6405
icarpio <ivancg24@gmail.com>
2019-10-11 18:02:47 +0200

    Add Insert & List

--- App.py
+++ App.py
@@ -1,11 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+
+
 
 app = Flask(__name__)
+app.secret_key = 'many random bytes'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'agenda'
+
+mysql = MySQL(app)
+
 
 
 @app.route('/')
 def Index():
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT  * FROM contactos")
+    data = cur.fetchall()
+    cur.close()
+
+
+
+
+    return render_template('index.html', contactos=data )
+
+
+
+@app.route('/insert', methods = ['POST'])
+def insert():
+
+    if request.method == "POST":
+        flash("Datos insertados correctamente")
+        name = request.form['nombre']
+        email = request.form['email']
+        phone = request.form['telefono']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO contactos (nombre, email, telefono) VALUES (%s, %s, %s)", (name, email, phone))
+        mysql.connection.commit()
+        return redirect(url_for('Index'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
