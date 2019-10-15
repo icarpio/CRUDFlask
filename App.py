015393e0fd27a97ddff448eb46e66887b73a457f
icarpio <ivancg24@gmail.com>
2019-10-15 11:30:09 +0200

    Add Update

--- App.py
+++ App.py
@@ -21,10 +21,6 @@ def Index():
     cur.execute("SELECT  * FROM contactos")
     data = cur.fetchall()
     cur.close()
-
-
-
-
     return render_template('index.html', contactos=data )
 
 
@@ -41,6 +37,21 @@ def insert():
         cur.execute("INSERT INTO contactos (nombre, email, telefono) VALUES (%s, %s, %s)", (name, email, phone))
         mysql.connection.commit()
         return redirect(url_for('Index'))
+
+
+@app.route('/update', methods = ['POST','GET'])
+def update():
+    if request.method == 'POST':
+        id_data = request.form['id']
+        name = request.form['nombre']
+        email = request.form['email']
+        phone = request.form['telefono']
+        cur = mysql.connection.cursor()
+        cur.execute("UPDATE contactos SET nombre=%s, email=%s, telefono=%s WHERE id=%s", (name,email,phone,id_data))
+        flash("Datos actualizados correctamente")
+        mysql.connection.commit()
+        return redirect(url_for('Index'))
+
 
 
 if __name__ == "__main__":
